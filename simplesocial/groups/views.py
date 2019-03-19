ea467dea7c93f47fabaffaf428a5a173334366b7
akdgtl <akostaroyale@gmail.com>
2019-03-19 15:47:27 +0200

    filled out all the group html pages and group views

--- simplesocial/groups/views.py
+++ simplesocial/groups/views.py
@@ -2,5 +2,22 @@
 from __future__ import unicode_literals
 
 from django.shortcuts import render
-
+from djang.contrib.auth.mixins import LoginRequiredMixin,PermissionRequiredMixin
+from django.core.urlresolvers import reverse
+from django.views import generic
+from groups.model import Group,GroupMember
 # Create your views here.
+
+class CreateGroup(LoginRequiredMixin,generic.CreateView):
+    fields = ('name','description')
+    models = Group
+
+class SingleGroup(generic.DetailView):
+    model = Group
+
+class ListGroups(generic.ListView):
+    model = Group
+    
+
+
+
